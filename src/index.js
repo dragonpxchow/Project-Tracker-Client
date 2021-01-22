057833fbfb10e24c98f884195746a61b14541b84
Dragon Chow <dragonpxchow@gmail.com>
2021-01-22 12:09:28 +1100

    add env.production

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import dotenv from "dotenv";
+
+//  loads the env variables into the process.env
+dotenv.config(); // from .env
+dotenv.config({ path: ".env." + process.env.NODE_ENV }); // from custome env file eg: .env.development or .env.production etc
 
 console.log("Application environment variables:", process.env.NODE_ENV);
 console.log("REACT_APP_API_URL >>>>", process.env.REACT_APP_API_URL);
