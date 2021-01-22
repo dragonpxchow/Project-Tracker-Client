f0dbc5be8917cc50118d6071eb837ee34a1eef19
Dragon Chow <dragonpxchow@gmail.com>
2021-01-23 00:57:00 +1100

    remove dotenv

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import dotenv from "dotenv";
-
-//  loads the env variables into the process.env
-dotenv.config(); // from .env
-dotenv.config({ path: ".env." + process.env.NODE_ENV }); // from custome env file eg: .env.development or .env.production etc
 
 console.log("Application client environment is:", process.env.NODE_ENV);
 console.log("REACT_APP_API_URL >>>>", process.env.REACT_APP_API_URL);
